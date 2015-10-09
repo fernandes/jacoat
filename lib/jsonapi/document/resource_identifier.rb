4d0a69c393941feb52c1c27084df0005d40a5edf
Celso Fernandes <fernandes@zertico.com>
2015-10-09 10:55:14 -0300

    Support relationships on Resource Identifier

--- lib/jsonapi/document/resource_identifier.rb
+++ lib/jsonapi/document/resource_identifier.rb
@@ -1,7 +1,7 @@
 module Jsonapi
   class Document
     class ResourceIdentifier
-      attr_reader :id, :type
+      attr_reader :id, :type, :relationships
       def self.process(body)
         if body.is_a?(Array)
           resources = []
@@ -17,13 +17,31 @@ module Jsonapi
       def initialize(arguments)
         @id = arguments[:id]
         @type = arguments[:type]
+        create_relationships(arguments[:relationships]) if arguments.has_key?(:relationships)
+      end
+
+      def create_relationships(hash)
+        @relationships = []
+        hash.each_pair do |k, v|
+          @relationships << Document::Relationship.new(k, v)
+        end
       end
       
       def to_hash
-        {
+        hash = {
           type: @type,
           id: @id
         }
+        hash.merge!(relationships: relationship_hash) if @relationships
+        hash
+      end
+      
+      def relationship_hash
+        hash = {}
+        @relationships.each do |relationship|
+          hash.merge!(relationship.to_hash)
+        end
+        hash
       end
     end
   end
--- spec/jsonapi/document/resource_identifier_spec.rb
+++ spec/jsonapi/document/resource_identifier_spec.rb
@@ -1,25 +1,98 @@
 require 'spec_helper'
 
 describe Jsonapi::Document::ResourceIdentifier do
-  let(:document) {
-    {
-      "type": "articles",
-      "id": "1"
+  context "with no relationships" do
+    let(:document) {
+      {
+        "type": "articles",
+        "id": "1"
+      }
     }
-  }
   
-  subject { described_class.new(document) }
-  describe "parses" do
-    it "id" do
-      expect(subject.id).to eq("1")
+    subject { described_class.process(document) }
+    describe "parses" do
+      it "id" do
+        expect(subject.id).to eq("1")
+      end
+      it "type" do
+        expect(subject.type).to eq("articles")
+      end
     end
-    it "type" do
-      expect(subject.type).to eq("articles")
+    context "render" do
+      it "hash document" do
+        expect(subject.to_hash).to eq(document)
+      end
     end
   end
-  context "render" do
-    it "hash document" do
-      expect(subject.to_hash).to eq(document)
+  
+  context "with a has one relationship" do
+    let(:document) {
+      {
+        "type": "articles",
+        "id": "1",
+        "relationships": {
+          "author": {
+            "data": { "type": "people", "id": "1" }
+          }
+        }
+      }
+    }
+  
+    subject { described_class.process(document) }
+    describe "parses" do
+      it "id" do
+        expect(subject.id).to eq("1")
+      end
+      it "type" do
+        expect(subject.type).to eq("articles")
+      end
+      it "relationship" do
+        expect(subject.relationships).to be_a(Array)
+        expect(subject.relationships.first).to be_a(Jsonapi::Document::Relationship)
+        expect(subject.relationships.first.type).to eq("author")
+      end
+    end
+    context "render" do
+      it "hash document" do
+        expect(subject.to_hash).to eq(document)
+      end
+    end
+  end
+  
+  context "with a has many relationship" do
+    let(:document) {
+      {
+        "type": "articles",
+        "id": "1",
+        "relationships": {
+          "tags": {
+            "data": [
+              { "type": "tags", "id": "2" },
+              { "type": "tags", "id": "3" }
+            ]
+          }
+        }
+      }
+    }
+  
+    subject { described_class.process(document) }
+    describe "parses" do
+      it "id" do
+        expect(subject.id).to eq("1")
+      end
+      it "type" do
+        expect(subject.type).to eq("articles")
+      end
+      it "relationship" do
+        expect(subject.relationships).to be_a(Array)
+        expect(subject.relationships.first).to be_a(Jsonapi::Document::Relationship)
+        expect(subject.relationships.first.type).to eq("tags")
+      end
+    end
+    context "render" do
+      it "hash document" do
+        expect(subject.to_hash).to eq(document)
+      end
     end
   end
 end
